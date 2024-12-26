8448eaa9abe06e4c72d56ab35fc825214aaa49d5
Kazuki Aishima <kazuki.aishima.dev@gmail.com>
2024-12-26 17:44:33 +0900

    サブページの追加

--- src/index.php
+++ src/index.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>test</title>
+    <title>練習用ページ</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<?php phpinfo(); ?>
+    <h1>ホームページ</h1>
+    <ul>
+        <li type="sqare">
+            <a href="./carrier.php">高専キャリア</a>
+        </li>
+    </ul>
 </body>
 </html>
