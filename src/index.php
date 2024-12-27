0cd18bf4c2434b054123fedab3c627275f5f463e
KabutoEbi <narenohatehe@gmail.com>
2024-12-27 17:08:58 +0900

    ツカモトのコミット

--- src/index.php
+++ src/index.php
@@ -12,6 +12,9 @@
         <li type="sqare">
             <a href="./carrier.php">高専キャリア</a>
         </li>
+        <li type="sqare">
+            <a href="./tsukamoto.php">test</a>
+        </li>
     </ul>
 </body>
 </html>
